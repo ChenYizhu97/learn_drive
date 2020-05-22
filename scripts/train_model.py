d2ebaf1231fc1212e382bf58c16035c30119afbe
chenyizhu97 <cyzhaa@gmail.com>
2020-05-22 22:54:33 +0200

    Modify th emodel

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,6 +1,7 @@
 import os
 import torch
 import torch.nn as nn
+import torchvision
 import numpy as np
 from PIL import Image
 from torch.utils.data import Dataset
@@ -8,17 +9,17 @@ from torch.utils.data import DataLoader
 from torchvision import transforms
 
 tsfm = transforms.Compose([
-            transforms.Resize((160, 160)),
+            transforms.Resize((224, 224)),
             transforms.ToTensor(),
-        ])
+        ]) 
 
 class MyTDataset(Dataset):
 
-    def __init__(self, images_dir='dataset/images', labels_dir='dataset/labels'):
+    def __init__(self, images_dir=os.path.join('dataset', 'images'), labels_dir=os.path.join('dataset', 'labels'), transform=tsfm):
         self.images_dir = images_dir
         self.T_labels = np.loadtxt(os.path.join(labels_dir, 'T_labels.csv'), delimiter='\n', dtype=np.float32)
         self.C_labels = np.loadtxt(os.path.join(labels_dir, 'C_labels.csv'), delimiter='\n', dtype=np.float32)
-        self.transform = tsfm
+        self.transform = transform
         super().__init__()
 
     def __len__(self):
@@ -32,36 +33,27 @@ class MyTDataset(Dataset):
         label = (self.T_labels[index], self.C_labels[index])
         return image, label
 
-model = nn.Sequential(
-        nn.Conv2d(3, 6, (6, 6)),
-        nn.Conv2d(6, 12, (6, 6)),
-        nn.MaxPool2d((5, 5)),
+model =  nn.Sequential(
+        torchvision.models.resnet18(pretrained=True),
+        nn.BatchNorm1d(1000),
         nn.ReLU(),
-        nn.Conv2d(12, 24, (4, 4)),
-        nn.Conv2d(24, 32, (4, 4)),
-        nn.MaxPool2d((3, 3)),
+        nn.Linear(1000, 512),
+        nn.BatchNorm1d(512),
         nn.ReLU(),
-        nn.Conv2d(32, 64, (3, 3)),
-        nn.Conv2d(64, 128, (3, 3)),
-        nn.MaxPool2d((2, 2)),
-        nn.ReLU(),
-        nn.Conv2d(128, 128, (2, 2)),
-        nn.Conv2d(128, 256, (1, 1)),
-        nn.Flatten(),
-        nn.BatchNorm1d(256),
-        nn.Linear(256, 128),
-        nn.ReLU(),
+        nn.Linear(512, 128),
         nn.BatchNorm1d(128),
+        nn.ReLU(),
         nn.Linear(128, 2),
         nn.Tanh(),
-        )
+        ).cuda()   
     
 if __name__ == "__main__":
        
-    dataset = MyTDataset()
-    dataloader = DataLoader(dataset, 128, shuffle=True)
+    #dataset = MyTDataset()
+    dataset = MyTDataset(transform=tsfm)
+    dataloader = DataLoader(dataset, 64, shuffle=True, num_workers=4)
     
-    L1_loss = nn.L1Loss()
+    L1_loss = nn.L1Loss().cuda()
 
     optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
 
@@ -69,24 +61,28 @@ if __name__ == "__main__":
     losses_C = []
 
     for epoch in range(0, 100):
-        loss_T = 0
-        loss_C = 0
-        for batch_id, (images, (label_T, label_C)) in enumerate(dataloader):
+        loss_T = torch.Tensor([0.0]).cuda()
+        loss_C = torch.Tensor([0.0]).cuda()
+        for batch_id, (images, (labels_T, labels_C)) in enumerate(dataloader):
+            batch_size = images.shape[0]
             optimizer.zero_grad()
+            images = images.cuda()
+            labels_C = labels_C.cuda()
+            labels_T = labels_T.cuda()
             predicts = model(images).reshape(-1, 2)
             predicts_T = predicts[:,0]
             predicts_C = predicts[:,1]
-            batch_loss_T = L1_loss(predicts_T, label_T)
-            batch_loss_C = L1_loss(predicts_C, label_C)
+            batch_loss_T = L1_loss(predicts_T, labels_T)
+            batch_loss_C = L1_loss(predicts_C, labels_C)
             batch_loss_C.backward(retain_graph=True)
             batch_loss_T.backward()
             optimizer.step()            
-            loss_T += batch_loss_T.detach().numpy().item()       
-            loss_C += batch_loss_C.detach().numpy().item()  
-        print('epoch {}, T loss is {:.5f}, C loss is {:.5f}'.format(epoch, loss_T, loss_C))
+            loss_T += batch_loss_T.detach() * batch_size             
+            loss_C += batch_loss_C.detach() * batch_size            
+            print('epoch {}, batch {}...\r'.format(epoch, batch_id), end='')
+        print('epoch {}, loss_T is {}, loss_C is {}'.format(epoch, loss_T, loss_C))
         losses_T.append(loss_T)
         losses_C.append(loss_C)    
     torch.save(model.state_dict(), 'model_state')
-
     print(losses_T)
     print(losses_C)
